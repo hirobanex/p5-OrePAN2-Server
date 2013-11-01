252aa66ad290effb99b0a855f2e4e2b9234a0f2d
Hiroyuki Akabane <hirobanex@gmail.com>
2013-11-01 17:31:18 +0900

    fix orepan2-server.pl document

--- script/orepan2-server.pl
+++ script/orepan2-server.pl
@@ -18,7 +18,7 @@ orepan2-server.pl - OrePAN2::Server launcher
         --delivery-dir=s     # a directory tar files of dist to be stored.       (Default: orepan)
         --delivery-path=s    # URL path behaves as cpan-mirror                   (Default: /orepan)
         --authenquery-path=s # URL path of the dist uploader                     (Default: /authenquery)
-        --compress-index     # 02packages.details.txt is to be compressed or not (Defualt: false)
+        --compress-index     # 02packages.details.txt is to be compressed or not (Defualt: true)
 
 =head1 DESCRIPTION
 
